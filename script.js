56e81dbb84c4b74a9fdaf34a0895cefcfad3d27f
Dev_Aurelien <lhuillier.aurelien47@gmail.com>
2025-01-02 23:48:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// Api Meteo 
+let api_key = 'f781d969fc0abdae456c5f7227c3772f'
+
+function changerVille(ville) {
+
+    const baseUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric`;
+    let requete = new XMLHttpRequest();
+    let url = baseUrl + "&q=" + ville + "&appid=" + api_key;
+    requete.open('GET', url);
+    requete.responseType = 'json';
+    requete.send();
+
+    requete.onload = function () {
+        if (requete.readyState === XMLHttpRequest.DONE) {
+            if (requete.status === 200) {
+                let reponse = requete.response;
+                document.getElementById('ville').textContent = reponse.name;
+                document.getElementById('temperature_label').textContent = reponse.main.temp;
+            }
+        }
+    }
+}
+
+changerVille("Paris");
+
+let btn = document.querySelector("#changer") 
+
+btn.addEventListener("click", () => {
+   let ville = prompt("Quel est la ville choisie ?");
+   changerVille(ville);
+})
+
